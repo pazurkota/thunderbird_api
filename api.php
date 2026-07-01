20a62a86585b336cfc0ad52dbf034cb7c99cf2f3
pazurkota <piotrek.jelonek@icloud.com>
2026-07-01 13:35:08 +0200

    Add PHPUnit configuration and initial test cases for account and message synchronization services

--- api.php
+++ api.php
@@ -1,3 +1,10 @@
 <?php
 
-require_once __DIR__ . '/Api/ThunderbirdApi.php';
+require_once __DIR__ . '/vendor/autoload.php';
+
+use App\Api\ThunderbirdApi;
+
+$secretToken = 'pazurkota_super_secret_api_key_2026';
+$sqliteDbPath = __DIR__ . '/Database/thunderbird.sqlite';
+
+ThunderbirdApi::create($secretToken, $sqliteDbPath)->run();
